9ce1ec56645a4f33851b687787e783d12bba196b
Lucas Arnström <lucas@arnstrom.se>
2022-08-10 12:09:13 +0200

    test the local file instead of the globally installed module

--- test_json_patch.py
+++ test_json_patch.py
@@ -2,7 +2,7 @@ from __future__ import (absolute_import, division, print_function)
 import json
 import pytest
 
-from ansible.modules.files.json_patch import JSONPatcher, PathError
+from json_patch import JSONPatcher, PathError
 
 __metaclass__ = type
 
